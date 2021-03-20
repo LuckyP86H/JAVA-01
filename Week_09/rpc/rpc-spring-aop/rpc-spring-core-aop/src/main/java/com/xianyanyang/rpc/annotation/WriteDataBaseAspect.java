6fa3f672d40917224fab31c6a639231914fda7b5
xianyanyang <xianyanyang>
2021-03-20 22:29:36 +0800

    第九周作业

---
+++ Week_09/rpc/rpc-spring-aop/rpc-spring-core-aop/src/main/java/com/xianyanyang/rpc/annotation/WriteDataBaseAspect.java
@@ -0,0 +1,24 @@
+package com.xianyanyang.rpc.annotation;
+
+import com.xianyanyang.config.annotation.DataBaseType;
+import com.xianyanyang.config.router.DataSourceRouterHolder;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 写数据库切面
+ */
+@Aspect
+@Component
+public class RpcServiceAspect {
+
+    @Around("@annotation(com.xianyanyang.config.annotation.wrire.WriteDataBase)")
+    public Object writeDataBase(ProceedingJoinPoint joinPoint) throws Throwable {
+        DataSourceRouterHolder.set(DataBaseType.write);
+        Object proceed = joinPoint.proceed();
+        DataSourceRouterHolder.clear();
+        return proceed;
+    }
+}
